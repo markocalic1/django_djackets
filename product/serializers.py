12ef032062edaf2356b662b469712eac3606bc13
markocalic1 <marko.calic@nava.hr>
2022-04-11 06:58:58 +0200

    first commit

---
+++ product/serializers.py
@@ -0,0 +1,33 @@
+from dataclasses import field, fields
+from re import T
+from rest_framework import serializers
+
+from .models import Category, Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail",
+
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+
+    class Meta:
+        model = Category
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "products",
+        )
